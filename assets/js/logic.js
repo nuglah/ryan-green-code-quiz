a8db847a3072737f963b328aa245829791392e7b
Ryan <thisianemailaddressthatis@gmail.com>
2022-07-10 21:32:51 -0500

    added some logic to logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,27 +1,54 @@
 var timerEl = document.getElementById("timer");
+var startQuizBtn = document.getElementById("start-button");
+var quiz = document.querySelector("#quiz");
 
 function countdown() {
   var timeLeft = 20;
 
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
+    var x = document.getElementById("start-quiz");
+    x.style.display = "none";
     if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
       timerEl.textContent = "Time: " + timeLeft;
-      // Decrement `timeLeft` by 1
       timeLeft--;
     } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
       timerEl.textContent = "Time: " + timeLeft;
       timeLeft--;
     } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
       timerEl.textContent = "";
-      // Use `clearInterval()` to stop the timer
       clearInterval(timeInterval);
     }
   }, 1000);
 }
 
-countdown();
+function renderQuiz(questionNumber) {
+  var question = questions[questionNumber].questionText;
+  var answer = questions[questionNumber].answer;
+  var displayWordElement = document.createElement("h3");
+  var button1 = document.createElement("button");
+
+  displayWordElement.textContent = question;
+  button1.textContent = questions[questionNumber].choices[0];
+  quiz.appendChild(displayWordElement);
+  quiz.appendChild(button1);
+
+  quiz.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (event.target.innerText === answer) {
+      //increment score
+    }
+    questionNumber++;
+    if (questionNumber < questions.length) {
+      renderQuiz(questionNumber);
+    } else {
+      // I am done show results write score to localstorage
+    }
+  });
+}
+
+startQuizBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  //check local storage for scores and sort score to get highest
+  countdown();
+  renderQuiz(0);
+});
